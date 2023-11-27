7f46e10b9c7efe2b8c9675690a84b864f3b3a010
gxldstein <gxldstein@tutanota.com>
2023-11-27 07:25:39 +0000

    3_for.py completed

--- 3_for.py
+++ 3_for.py
@@ -16,12 +16,35 @@
 * Посчитать и вывести среднее количество продаж всех товаров
 """
 
+from statistics import mean
+
+
 def main():
-    """
-    Эта функция вызывается автоматически при запуске скрипта в консоли
-    В ней надо заменить pass на ваш код
-    """
-    pass
-    
+    phones_list = [
+        {
+            "product": "iPhone 12",
+            "items_sold": [363, 500, 224, 358, 480, 476, 470, 216, 270, 388, 312, 186],
+        },
+        {
+            "product": "Xiaomi Mi11",
+            "items_sold": [317, 267, 290, 431, 211, 354, 276, 526, 141, 453, 510, 316],
+        },
+        {
+            "product": "Samsung Galaxy 21",
+            "items_sold": [343, 390, 238, 437, 214, 494, 441, 518, 212, 288, 272, 247],
+        },
+    ]
+    mean_all = 0
+    sum_all = 0
+    for phone in phones_list:
+        m = mean(phone["items_sold"])
+        s = sum(phone["items_sold"])
+        print(f'{phone["product"]}: sum items sold {s}, mean items sold {round(m, 2)}')
+        mean_all += m
+        sum_all += s
+    print(f"sum all sold: {sum_all / len(phones_list):.2f}")
+    print(f"mean all sold: {mean_all / len(phones_list):.2f}")
+
+
 if __name__ == "__main__":
     main()
